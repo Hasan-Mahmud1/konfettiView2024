a46b114c4142a61bc631bf4d5807ac19df0a17e3
Hasan-Mahmud1 <mrj6458@gmail.com>
2024-10-06 16:14:20 +0600

    first commit

---
+++ app/src/main/java/com/example/confettiapplication/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.confettiapplication
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import nl.dionsegijn.konfetti.core.Party
+import nl.dionsegijn.konfetti.core.Position
+import nl.dionsegijn.konfetti.core.emitter.Emitter
+import nl.dionsegijn.konfetti.xml.KonfettiView
+import java.util.concurrent.TimeUnit
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val konfettiView = findViewById<KonfettiView>(R.id.konfettiView)
+        val playBtn: Button = findViewById(R.id.playBtn)
+
+        val party= Party(
+            speed = 0f,
+            maxSpeed = 30f,
+            damping = 0.9f,
+            spread = 360,
+            colors = listOf(0xfce18a, 0xff726d, 0xf4306d, 0xb48def),
+            position = Position.Relative(0.5, 0.3),
+            emitter = Emitter(duration = 100, TimeUnit.MILLISECONDS).max(100)
+        )
+
+        playBtn.setOnClickListener {
+            konfettiView.start(party)
+        }
+    }
+}
